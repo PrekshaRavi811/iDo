47e151ed6c8a8b096589d3337ed6941e9c14fc55
diya-bera <54590394+diya-bera@users.noreply.github.com>
2021-03-25 17:43:33 -0400

    committing to branch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: ''
+        };
+    }
+
+    handleNameOnChange(e) {
+        this.setState({
+            name: e.target.value,
+        });
+    }
+
+    handleCuisineOnChange(e) {
+        this.setState({
+            cuisine: e.target.value,
+        })
+    }
+
+    handlePhoneOnChange(e) {
+        this.setState({
+            phone: e.target.value,
+        })
+    }
+
+    handlePriceOnChange(e) {
+        this.setState({
+            price: e.target.value,
+        })
+    }
+
+    addToDatabase() {
+
+    }
+
+    render() {
+        return (
+            <div className="wrap">
+                <h1 className="string"> Name </h1>
+                <input type="text" id="name" value={this.state.name} onChange={ (e) => this.handleNameOnChange(e) } />
+                <h1 className="string">Cuisine</h1>
+                <input type="text" id="cuisine" value={this.state.cuisine} onChange={ (e) => this.handleCuisineOnChange(e) }/>
+                <h1 className="string">Phone Number</h1>
+                <input type="number" id="phone" value={this.state.phone} onChange={ (e) => this.handlePhoneOnChange(e) }/>
+                <h1 className="string">Price Per Guest</h1>
+                <input type="number" id="price" value={this.state.price} onChange={ (e) => this.handlePriceOnChange(e) }/>
+
+                <h1 className="string"> Hi {this.state.name}! You provide {this.state.cuisine} food and your contact number is {this.state.phone}. Your average price per guest is ${this.state.price}. Is your information correct? </h1>
+
+                <button onClick={this.addToDatabase()}> SUBMIT </button>
+
+            </div>
+        )
+
+    }
 }
 
 export default App;
