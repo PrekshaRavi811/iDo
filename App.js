1a2926419a5df25025b15a6c953771e54874bbc9
Akshi Jain <54599561+akshi-j@users.noreply.github.com>
2021-04-18 14:44:25 -0400

    Added Links

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+//const connection = require('./routes');
+import display from './display';
+import showTable from './showTable';
+import home from './home';
+import {Route, Link} from "react-router-dom";
+import NavBar from './NavBar';
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <NavBar />
+                <Route exact path="/" component={home} />
+                <Route exact path="/getForm" component={display} />
+                <Route exact path="/showTable" component={showTable} />
+
+            </div>
+        );
+    };
+}
+
+export default App;
