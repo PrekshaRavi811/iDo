6181337f869ef4cff60e31e3e1f87f7b7521f5a1
PrekshaRavi811 <ptharigo@purdue.edu>
2021-05-05 08:44:47 -0400

    cake display fix1

--- src/Customer.js
+++ src/Customer.js
@@ -14,7 +14,7 @@
 
 import React from 'react';
 import Food from './FoodDisplay';
-// import Cake from './components/CakeCompany';
+import Cake from './CakeDisplay';
 // import Dress from './components/DressCompany';
 // import Venue from './components/VenueCompany';
 // import Entertainment from "./components/EntertainmentCompany";
